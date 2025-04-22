4126d1861cf059a48516355ca196290d8557a780
Alfredo Isaias Noriega Aranda <alfredonoriega98765@alumnes.ilerna.com>
2025-04-22 12:58:04 +0200

    Agregue el javaDoc al proyecto

---
+++ Dambnb-g-2/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"daxbnb.DAO"},{"l":"daxbnb.loadData"},{"l":"daxbnb.loadScript"},{"l":"daxbnb.model"}];updateSearchResults();
