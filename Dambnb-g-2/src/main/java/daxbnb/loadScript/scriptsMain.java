48380540c849924b777a06f32c29f52b0bc3c5e3
Your Name <you@example.com>
2025-05-06 10:26:45 +0200

    Nuevos cambios en JavaDocs

--- Dambnb-g-2/src/main/java/daxbnb/loadScript/scriptsMain.java
+++ Dambnb-g-2/src/main/java/daxbnb/loadScript/scriptsMain.java
@@ -9,14 +9,14 @@ import daxbnb.DAO.DBConnection;
 
 public class scriptsMain {
 
-public static void main(String[] args) {
+	public static void main(String[] args) {
 		try {
 			String sql = new String(Files.readAllBytes(Paths.get("scripts/CreateTableBBDD.sql")));
 			DBConnection dbConnection = new DBConnection();
 			Connection connection = dbConnection.connect(); // Abrimos la conexion
 			Statement stmt = connection.createStatement(); // Creamos el statement que se crea a partir de la conexion y
 															// sirve para enviar instruciones SQL
-			stmt.execute(sql);//Aqui ejecutamos el sql con el statement
+			stmt.execute(sql);// Aqui ejecutamos el sql con el statement
 			System.out.println("Base de datos creada correctamente.");
 
 			dbConnection.closeConnection(connection); // cerramos la conexion
